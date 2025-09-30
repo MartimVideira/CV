8e328ae6e07de0c5171e64db8a67b9001ca6a33f
MartimVideira <mitram790@gmail.com>
2025-09-30 16:25:09 +0100

    Experimenting building static site with php and respective action

---
+++ page/index.php
@@ -0,0 +1,20 @@
+<?php
+    $data = ["prefered_name" => "Martim Videira"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?php echo $data["prefered_name"] ?> </title>
+</head>
+<body>
+    <h1>CV <?php echo $data["prefered_name"] ?> </h1>
+    <section id="education" >
+    </section>
+    <section id="experience" >
+    </section>
+    <section id="projects">
+    </section>
+</body>
+</html>
